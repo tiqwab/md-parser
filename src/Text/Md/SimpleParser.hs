50b4cb3d2c778640a8cfa5c4c563de344d97444d
tiqwab <tiqwab.ch90@gmail.com>
2016-10-18 08:31:49 +0900

    Handle empty line

--- src/Text/Md/SimpleParser.hs
+++ src/Text/Md/SimpleParser.hs
@@ -30,25 +30,33 @@ pBody = do parts <- P.many P.letter `P.sepBy` P.count 2 P.newline
 -}
 
 pDocument = do x <- pParagraph
-               y <- P.optionMaybe (pEmptyLine *> pDocument)
+               y <- P.optionMaybe (pNewLine *> pDocument)
                case y of
                  Nothing -> return [x]
                  Just z  -> return (x : z)
 
 pParagraph = P.many1 pLine
 
-pEmptyLine = P.skipMany (P.char ' ' <|> P.char '\t') *> P.newline
-
 pLine = P.many1 (P.noneOf "\n") <* pNewLine
 
 pNewLine = P.newline
 
+-- Process input before parsing.
+preProcess :: String -> String
+preProcess = unlines . map trimEmptyLine . lines
+  where trimEmptyLine s
+          | isEmptyLine s = ""
+          | otherwise     = s
+        isEmptyLine = foldr (\x -> (&&) (x `elem` ['\n', '\t', ' '])) True
+
+-- Parse Markdown to HTML string.
 parseMd :: String -> String
-parseMd input = let parsed = P.parse pDocument "" input
+parseMd input = let parsed = P.parse pDocument "" (preProcess input)
                 in case parsed of
                   Left  e      -> show e
                   Right output -> show output
 
+-- Parse Markdown in a file to HTML string.
 parseMdFile :: FilePath -> IO String
 parseMdFile file = withFile file ReadMode $ \handle -> do
   contents <- hGetContents handle
