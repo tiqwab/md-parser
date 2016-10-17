6292380708d9f63165870694cff03359b5d82750
tiqwab <tiqwab.ch90@gmail.com>
2016-10-18 08:32:28 +0900

    Remove unnecessary functions

--- src/Text/Md/SimpleParser.hs
+++ src/Text/Md/SimpleParser.hs
@@ -8,27 +8,6 @@ import           Text.Parsec                   ((<|>))
 import qualified Text.Parsec                   as P
 import qualified Text.ParserCombinators.Parsec as P hiding (try)
 
-{-
-pBody = codeBody <$> (P.many P.anyChar <* P.notFollowedBy (P.char '\n')) `P.sepBy` pNewLines
-  where codeBody = foldl1 (++) . map (\s -> "<p>" ++ s ++ "</p>")
-        pNewLines = P.newline *> P.many1 P.newline
--}
-
-{-
-pBody = sequence [pParagraph, pParagraph]
-
-pParagraph = codeParagraph <$> (  P.try (P.manyTill P.anyChar pNewLines)
-                              <|> P.many P.anyChar  )
-  where codeParagraph s = "<p>" ++ s ++ "</p>"
-        pNewLines  = P.try (P.newline *> P.many1 P.newline)
--}
-
-{-
-pBody = do parts <- P.many P.letter `P.sepBy` P.count 2 P.newline
-           sequence . fmap pId $ parts
-  where pId s = return ("<p>" ++ s ++ "</p>")
--}
-
 pDocument = do x <- pParagraph
                y <- P.optionMaybe (pNewLine *> pDocument)
                case y of
