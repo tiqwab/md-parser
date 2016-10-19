bbf1effeb37b2fa6eee486c0918e07d389adb289
tiqwab <tiqwab.ch90@gmail.com>
2016-10-19 22:23:14 +0900

    Add simple list

--- memo.md
+++ memo.md
@@ -2,15 +2,28 @@
 
 - とりあえず改行コードは`\n`だけ考える。前処理で統一されるよう変換もできるので。
 - 各行の末尾にnewlineがあることが前提になっている。
+- いくつかのタグでMarkdown仕様上では表記法が複数存在するが、省略しているものがある。
+  - border: '-'のみ
+  - list: '-'のみ
+- ブロックタブの一部でうまく改行を処理できない(致命的)
+  - list
+  - code
 
 document  = paragraph, {(emptyLine, paragraph)}
 paragraph = line, {line}
-emptyLine = {(space | tab)}, newline
+emptyLine = {spaces}, newline
 line      = anyChar, {anyChar}, newline
 newline   = "\n"
 space     = " "
 tab       = '\t'
+spaces    = (space | tab)
 anyChar   = (any character but newline)
+
+head      = "#", {"#"}, anyChar, newline
+border    = ("-", {spaces}, "-", {spaces}, "-"), {("-" | spaces)}, newline
+list      = listItem, {listItem}
+listVal   = {spaces}, (anyChar but "-"), {anyChar}
+listItem  = {spaces}, "-", spaces, (listVal, newline), {(listVal, newline)}
 
 ---
 
@@ -25,7 +38,7 @@ Nice to meet you.
 
 ---
 
-<p>Hi, 
+<p>Hi,
 I am Taro.</p>
 
 <ul>
--- src/Text/Md/SimpleParser.hs
+++ src/Text/Md/SimpleParser.hs
@@ -4,9 +4,11 @@ module Text.Md.SimpleParser
 where
 
 import           System.IO
-import           Text.Parsec                   ((<|>))
+import           Text.Parsec                   (Parsec, ParsecT, (<|>))
 import qualified Text.Parsec                   as P
 import qualified Text.ParserCombinators.Parsec as P hiding (try)
+
+{- Parse document -}
 
 pDocument = do x <- pParagraph
                y <- P.optionMaybe (pNewLine *> pDocument)
@@ -21,21 +23,36 @@ pLine = P.many1 (P.noneOf "\n") <* pNewLine
 
 pNewLine = P.newline
 
+-- P.space also accepts newline
+pSpaces  = P.char ' ' <|> P.char '\t'
+
 {- Parse block tags -}
 
 -- Is it good way to parse again?
 parseBlock input = case P.parse pBlock "" input of
                      Left  l -> "<p>" ++ trim input ++ "</p>"
                      Right r -> r
-  where pBlock =  pHead
-              <|> pBorder
+  where pBlock =  P.try pHead
+              <|> P.try pBorder
+              <|> P.try pList
 
-pHead = do P.char '#'
+pHead = do hs <- countH <$> P.many1 (P.char '#')
            x <- P.many1 (P.noneOf "\n")
-           return $ "<h1>" ++ trim x ++ "</h1>"
+           return $ "<h" ++ hs ++ ">" ++ trim x ++ "</h" ++ hs ++">"
+  where countH x = let c = length x
+                   in show (min c 6)
 
-pBorder = do P.count 3 (P.char '-')
-             return "<hr/>"
+pBorder = do bcount <- countB <$> P.many (P.char '-' <* P.many pSpaces)
+             P.newline
+             if bcount >= 3 then return "<hr/>" else fail "this is not border"
+  where countB = length
+
+pList = do xs <- P.many1 pListItem
+           let items =  foldr1 (++) . map (\x -> "<li>" ++ x ++ "</li>") $ xs
+           return $ "<ul>" ++ items ++ "</ul>"
+  where pListItem = do P.char '-'
+                       P.space
+                       pLine
 
 {- Utils -}
 
